08c9e1e6e3adc1319965317e99f04c03f0b3e1c7
Tbryant7 <tannonbryant@gmail.com>
2024-02-28 19:45:39 -0500

    Added starter code

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,5 +1,20 @@
+/*Tannon Bryant
+ * 2/28/24
+ * Dmv simulator
+ */
+
+
+ import java.util.Random;
 
 public class DmvSimulator {
 public static void main(String args[]) {
+
+System.out.println("Welcome to the DMV!");
+Random random = new Random();
+int userNumber = random.nextInt(100) + 1;
+System.out.println("Your number is: " + userNumber + "Please wait untik you are called");
+
+
+int currentNumber = userNumber % + 1;
 }
 }
