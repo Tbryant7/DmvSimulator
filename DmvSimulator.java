8ccaed40af7b56ddd5884b1d39048bff1488d169
Tbryant7 <tannonbryant@gmail.com>
2024-03-07 13:45:11 -0500

    finshed the main code

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -16,5 +16,17 @@ System.out.println("Your number is: " + userNumber + "Please wait untik you are 
 
 
 int currentNumber = userNumber % + 1;
+System.out.println("Your number is: " + userNumber + ". Please wait until your number is called.");
+
+        // Calling out numbers until the user's number is called
+        currentNumber = userNumber % 100 + 1;
+        while (currentNumber != userNumber) {
+            System.out.println("Now serving number: " + currentNumber);
+            currentNumber = (currentNumber % 100) + 1; // Wrapping around to 1 if currentNumber equals 100
+        }
+
+        // Notifying the user about the paperwork
+        System.out.println("Your number " + userNumber + " is called. Please proceed to the counter.");
+        System.out.println("Sorry, you don't have the required paperwork.");
 }
 }
